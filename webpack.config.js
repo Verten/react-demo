caa5438429e727a0582ca8a4ec6bf254294c6b7f
ebinhon <bin.hong@ericsson.com>
2016-05-04 15:23:19 +0800

    add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 var path = require('path');
+var webpack = require('webpack');
 var node_modules = path.resolve(__dirname, 'node_modules');
 var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+var pathToReactDOM = path.resolve(node_modules, 'react-dom/dist/react-dom.min.js');
 
 var debug = process.env.NODE_ENV !== "production"; // or dev
 
 
 module.exports = {
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   entry: debug ? ['webpack/hot/dev-server', path.resolve(__dirname, './app/main.js')] : [path.resolve(__dirname, './app/main.js')],
   resolve: {
     extensions: ['', '.js', '.json', '.scss'],
     alias: {
-      'react': pathToReact
+      'react': pathToReact,
+      'react-dom': pathToReactDOM
     }
   },
   output: {
@@ -22,6 +28,6 @@ module.exports = {
       test: /\.jsx?$/,
       loader: 'babel'
     }],
-    noParse:[pathToReact]
+    noParse:[pathToReact,pathToReactDOM]
   }
 };
