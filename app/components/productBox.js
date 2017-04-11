0385cfb07103b34ff5ab528c9a1112aef3c10bcf
ebinhon <bin.hong@ericsson.com>
2017-04-11 15:59:38 +0800

    add hot module replace

--- app/components/productBox.js
+++ app/components/productBox.js
@@ -1,35 +1,33 @@
 import React from 'react';
 
 export default class productBox extends React.Component {
-    static propTypes = {
-        //React.PropTypes.string.isRequired,
-        //React.PropTypes.bool,
-        //React.PropTypes.object,
-        //React.PropTypes.oneOf(['value1', 'value2'])
-        //reference to official URL: https://facebook.github.io/react/docs/reusable-components.html
+  static propTypes = {
+    //React.PropTypes.string.isRequired,
+    //React.PropTypes.bool,
+    //React.PropTypes.object,
+    //React.PropTypes.oneOf(['value1', 'value2'])
+    //reference to official URL: https://facebook.github.io/react/docs/reusable-components.html
 
-    }
+  }
 
-    static defaultProps = {
+  static defaultProps = {}
 
-    }
+  constructor() {
+    super();
+    this.state = {}
+  }
 
-    constructor() {
-        super();
-        this.state = {
+  componentDidMount() {
 
-        }
-    }
+  }
 
-    componentDidMount(){
-
-    }
-
-    render() {
-        return(
-            <div className="productBox">
-                Hello World!
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="productBox">
+        Hello World!
+        <br/>
+        Hello React!
+      </div>
+    );
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 var path = require('path');
 var webpack = require('webpack');
 var node_modules = path.resolve(__dirname, 'node_modules');
-var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
-var pathToReactDOM = path.resolve(node_modules, 'react-dom/dist/react-dom.min.js');
 
-var debug = process.env.NODE_ENV !== "production"; // or dev
-
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './app/index.html'),
+    }),
   ],
-  entry: debug ? ['webpack/hot/dev-server', path.resolve(__dirname, './app/main.js')] : [path.resolve(__dirname, './app/main.js')],
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080/',
+    path.resolve(__dirname, './app/main.js'),
+  ],
   resolve: {
     extensions: ['', '.js', '.json', '.scss'],
-    alias: {
-      'react': pathToReact,
-      'react-dom': pathToReactDOM
-    }
   },
   output: {
     path: path.resolve(__dirname, './build'),
@@ -26,8 +26,11 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loader: 'babel'
-    }],
-    noParse:[pathToReact,pathToReactDOM]
+      exclude: path.resolve(__dirname, 'node_modules'),
+      loaders: [
+        'react-hot-loader',
+        'babel-loader'
+      ]
+    }]
   }
 };
