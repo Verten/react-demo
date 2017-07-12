b481e205e717fe243eafbce2db82b5fc091f9e6c
ebinhon <bin.hong@ericsson.com>
2017-07-12 14:54:06 +0800

    delete comment code

--- src/components/productBox.js
+++ src/components/productBox.js
@@ -1,14 +1,7 @@
 import React from 'react';
 
 export default class productBox extends React.Component {
-  static propTypes = {
-    //React.PropTypes.string.isRequired,
-    //React.PropTypes.bool,
-    //React.PropTypes.object,
-    //React.PropTypes.oneOf(['value1', 'value2'])
-    //reference to official URL: https://facebook.github.io/react/docs/reusable-components.html
-
-  }
+  static propTypes = {}
 
   static defaultProps = {}
 
